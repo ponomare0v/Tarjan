1b07becc834b78b849753fa1c6e35cfee22db166
ponomare0v <161859675+ponomare0v@users.noreply.github.com>
2024-06-16 01:43:15 +0300

    third commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // Включаем CORS для конкретного домена
   app.enableCors({
     origin: 'http://127.0.0.1:5500', // Разрешить запросы только с этого домена
@@ -10,6 +12,8 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
+  // Настройте путь к статическим файлам
+  app.useStaticAssets(join(__dirname, '..', 'client')); // теперь по адресу http://localhost:3000/ будет отрываться index.html
   await app.listen(3000);
 }
 bootstrap();
